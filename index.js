45d8f76b0faf1cdad9ce32713d8c1e323c2e01ef
Jason Pao <yoyoonarock@gmail.com>
2017-05-06 10:37:31 -0700

    Read from the PORT that Heroku sets.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const request = require('request');
 const bodyParser = require('body-parser');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(bodyParser.json());
